88abb16c31335fcc9c5e7257d7ddad80cb0e0d7b
luizzcarlos <juninho_2014br@hotmail.com>
2019-09-18 22:32:51 -0300

    Calculo IMC

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,9 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'hello world';
   foto = 'favicon.icon';
+  peso = 0;
+  altura = 0;
   
-
   exibir(){
     alert(this.title);
+  }
+
+  
+  IMC(){
+    let calculo = this.peso/(this.altura*this.altura);
+      alert (calculo);
   }
 }
